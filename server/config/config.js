189fad7e23b3c9b061d18f029f2337a797b90449
Adrian <orzadr@gmail.com>
2019-06-16 18:19:24 -0500

    message00

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,8 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 
 } else {
-    urlDB = process.env.MONGO_URI;
+    urlDB = process.env.MONGO_URL;
+
 }
 
 process.env.URLDB = urlDB;
